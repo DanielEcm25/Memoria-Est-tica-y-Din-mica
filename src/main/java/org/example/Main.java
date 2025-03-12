e61f313010b0683c9bb842681353703260b72381
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-03-11 21:02:51 -0500

    Prueba de conexión

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("El Repositorio se ha podido enviar con exito");
+        System.out.println("Prueba de conexión");
     }
 }
