bed38fce294371f6013305ce6858be23ab5e38d7
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-03-11 19:48:58 -0500

    Nuevo enlace

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("El Repositorio se ha podido enviar con exito");
         System.out.println("Modificación ligera ");
+        System.out.println("Confirmando cambio de nombre del repo");
     }
 }
