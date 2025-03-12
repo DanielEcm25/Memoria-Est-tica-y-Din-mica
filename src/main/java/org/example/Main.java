238b623aa89163c82e52d0adc09354a24ed59eb1
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-03-11 21:00:01 -0500

    Iniciando Recursividad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,5 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("El Repositorio se ha podido enviar con exito");
-        System.out.println("Modificación ligera ");
-        System.out.println("Confirmando cambio de nombre del repo");
     }
 }
